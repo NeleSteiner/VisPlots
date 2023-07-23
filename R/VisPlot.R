24c6fea4fa9b23ada6bb2ed15a609a45ca9ac836
bichmaivu <B.Vu@campus.lmu.de>
2023-07-23 15:11:07 +0200

    spelling mistake

--- R/VisPlot.R
+++ R/VisPlot.R
@@ -7,7 +7,7 @@
 #' @description Creates a scatter plot using ggplot2.
 #' Discover relationships and patterns between two numerical variables with a
 #' scatter plot, ideal for understanding correlations and identifying outliers.
-#' @title Scatter plot
+#' @title Scatter Plot
 #' @param data A data.frame or any other object provided will be used to replace
 #' the default plot data. All objects will be transformed into a fortified
 #' data.frame. You can refer to the fortify() function to understand which
@@ -49,7 +49,7 @@ scatter_plot <- function(data = NULL, x, y, xlab = "x-axis", ylab = "y-axis",
 #' @description Creates a jitter plot using ggplot2.
 #' Uncover the distribution of data points along a single axis, revealing the
 #' density and spread of values, particularly useful for smaller datasets.
-#' @title Jitter plot
+#' @title Jitter Plot
 #' @param data A data.frame or any other object provided will be used to replace
 #' the default plot data. All objects will be transformed into a fortified
 #' data.frame. You can refer to the fortify() function to understand which
@@ -94,7 +94,7 @@ jitter_plot <- function(data = NULL, x, y, xlab = "x-axis", ylab = "y-axis",
 }
 
 #' @description Creates a jitter plot with different shapes using ggplot2.
-#' @title Jitter plot with different shapes
+#' @title Jitter Plot with different shapes
 #' @param data A data.frame or any other object provided will be used to replace
 #' the default plot data. All objects will be transformed into a fortified
 #' data.frame. You can refer to the fortify() function to understand which
@@ -136,7 +136,7 @@ jitter_plot <- function(data = NULL, x, y, xlab = "x-axis", ylab = "y-axis",
 
 jitter_plot_shape <- function(data = NULL, x, y, xlab = "x-axis", ylab = "y-axis",
                              shape = NULL, colour = NULL,
-                             title = "Jitter plot with point shapes") {
+                             title = "Jitter Plot with different shapes") {
   library(ggplot2)
   ggplot(data = NULL, aes(x = {{x}}, y = {{y}})) +
     geom_point(size = NULL, color = NULL) +
@@ -148,7 +148,7 @@ jitter_plot_shape <- function(data = NULL, x, y, xlab = "x-axis", ylab = "y-axis
 #' @description Creates a line chart using ggplot2.
 #' Visualise trends and changes in data over continuous intervals, perfect for
 #' time-series data or any dataset with a natural ordering.
-#' @title Line chart
+#' @title Line Chart
 #' @param data A data.frame or any other object provided will be used to replace
 #' the default plot data. All objects will be transformed into a fortified
 #' data.frame. You can refer to the fortify() function to understand which
@@ -189,7 +189,7 @@ jitter_plot_shape <- function(data = NULL, x, y, xlab = "x-axis", ylab = "y-axis
 #'             title = "beer consumption during the Oktoberfest")
 
 line_chart <- function(data = NULL, x, y, xlab = "x-axis", ylab = "y-axis",
-                       colour = "blue", title = "Line chart") {
+                       colour = "blue", title = "Line Chart") {
   library(ggplot2)
   ggplot(data = NULL, aes(x = {{x}}, y = {{y}})) +
   geom_line(color = NULL) +
